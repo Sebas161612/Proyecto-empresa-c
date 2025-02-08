6c0768b50de8a2338b52bffb3a6ca93a89d3d5f2
Sebas161612 <sebastianflow111@gmail.com>
2025-02-08 10:53:27 -0500

    aguacate

--- hola.py
+++ hola.py
@@ -1,9 +1,12 @@
 #hola pythoneros
-
+m = 6
 #nose que decir
 #Como vamos chavales
 #porque no sirve
-#ojala
+if m <= 5:
+    print ("Es alto")
+else:
+    print ("aguacate")
 #sin palabras
 #espero sirva
 #de verdad sirve
